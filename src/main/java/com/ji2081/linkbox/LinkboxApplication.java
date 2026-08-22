1a27d6bbfca12304dc058cce404a2986bcb920d8
ji2081 <ji2081_@naver.com>
2026-08-23 05:19:41 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/ji2081/linkbox/LinkboxApplication.java
@@ -0,0 +1,13 @@
+package com.ji2081.linkbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ji2081/linkbox/LinkboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ji2081.linkbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

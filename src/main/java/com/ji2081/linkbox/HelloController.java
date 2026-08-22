1f07c4420fe2c7cf05564f66e9dcc08a4ee056d3
ji2081 <ji2081_@naver.com>
2026-08-23 05:29:35 +0900

    feat: 헬스체크용 hello 엔드포인트 추가

---
+++ src/main/java/com/ji2081/linkbox/HelloController.java
@@ -0,0 +1,14 @@
+package com.ji2081.linkbox;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "안녕하세요, linkbox입니다!";
+    }
+}
+
